a15459e3bbfd1cee4c7e8d8bda5c11d2329c7674
Sebastian Kürten <sebastian@topobyte.de>
2021-02-13 16:07:22 +0100

    Zeige Dreiergruppen in Liste an

--- project/web/src/main/java/de/mobanisto/gartendaten/pages/other/ListeGenerator.java
+++ project/web/src/main/java/de/mobanisto/gartendaten/pages/other/ListeGenerator.java
@@ -1,9 +1,19 @@
 package de.mobanisto.gartendaten.pages.other;
 
+import static de.mobanisto.gartendaten.Fit.GOOD;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Table;
 
 import de.mobanisto.gartendaten.Data;
+import de.mobanisto.gartendaten.Fit;
 import de.mobanisto.gartendaten.Plant;
 import de.mobanisto.gartendaten.Website;
 import de.mobanisto.gartendaten.pages.base.SimpleBaseGenerator;
@@ -19,11 +29,13 @@ public class ListeGenerator extends SimpleBaseGenerator
 {
 
 	private String listName;
+	private Data data;
 
 	public ListeGenerator(WebPath path, String listName)
 	{
 		super(path);
 		this.listName = listName;
+		data = Website.INSTANCE.getData();
 	}
 
 	@Override
@@ -33,23 +45,88 @@ public class ListeGenerator extends SimpleBaseGenerator
 
 		ListGroupDiv list = content.ac(Bootstrap.listGroupDiv());
 
-		Data data = Website.INSTANCE.getData();
 		List<String> names = data.getNames().get(listName);
 		if (names == null) {
 			throw new PageNotFoundException();
 		}
 
-		Map<String, Plant> plants = data.getPlants();
+		Map<String, Plant> plantLookup = data.getPlants();
+
+		List<Plant> plants = new ArrayList<>();
 
 		for (String plantName : names) {
-			Plant plant = plants.get(plantName);
+			Plant plant = plantLookup.get(plantName);
 			ListGroupA item = list.addA("/pflanze/" + plantName, plantName);
 			if (plant == null) {
 				item.setContext(ContextualType.DANGER);
 			} else {
+				plants.add(plant);
 				item.setContext(ContextualType.SUCCESS);
 			}
 		}
+
+		group(plants);
+	}
+
+	private void group(List<Plant> plants)
+	{
+		int n = plants.size();
+
+		List<List<Plant>> three = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				for (int k = j; k < n; k++) {
+					if (check(plants, i, j, k)) {
+						three.add(set(plants, i, j, k));
+					}
+				}
+			}
+		}
+
+		Set<Plant> got = new HashSet<>();
+
+		content.ac(HTML.h2("Dreiergruppen")).addClass("mt-3");
+		ListGroupDiv list = content.ac(Bootstrap.listGroupDiv());
+		for (List<Plant> group : three) {
+			list.addTextItem(Joiner.on(", ")
+					.join(Iterables.transform(group, Plant::getName)));
+			for (Plant plant : group) {
+				got.add(plant);
+			}
+		}
+
+		content.ac(HTML.h2("Übrig")).addClass("mt-3");
+		list = content.ac(Bootstrap.listGroupDiv());
+		for (Plant plant : plants) {
+			if (got.contains(plant)) {
+				continue;
+			}
+			list.addTextItem(plant.getName());
+		}
+	}
+
+	private List<Plant> set(List<Plant> plants, int i, int j, int k)
+	{
+		List<Plant> selected = new ArrayList<>();
+		selected.add(plants.get(i));
+		selected.add(plants.get(j));
+		selected.add(plants.get(k));
+		return selected;
+	}
+
+	private boolean check(List<Plant> plants, int i, int j, int k)
+	{
+		Plant pi = plants.get(i);
+		Plant pj = plants.get(j);
+		Plant pk = plants.get(k);
+		Table<Plant, Plant, Fit> mix = data.getMix();
+		Fit fit1 = mix.get(pi, pj);
+		Fit fit2 = mix.get(pi, pk);
+		Fit fit3 = mix.get(pj, pk);
+		if (fit1 == GOOD && fit2 == GOOD && fit3 == GOOD) {
+			return true;
+		}
+		return false;
 	}
 
 }
